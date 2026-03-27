530a2d1275ec98553d8b73f3827b49dede37df5c
komiljonovd <komiljonooov.d@gmail.com>
2026-03-28 01:43:43 +0500

    fix

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,36 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Alpha API",
+      default_version='v1',
+      description="Alpha description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('userapp.urls')),
     path('note/',include('noteapp.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    path('swagger/<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
